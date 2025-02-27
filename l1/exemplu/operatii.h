12c26fb90b1c3c51e99f810f77748d253b75e3fd
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-27 22:33:58 +0200

    a

--- l1/exemplu/operatii.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPERATII_H_
-#define OPERATII_H_
-
-double mediaAritmetica(int, int);
-double mediaGeometrica(int, int);
-void help(void);
-
-#endif
-
--- l1/p2.2/integrala.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTEGRALA_H_
-#define INTEGRALA_H_
-
-double functie(double x);
-double integralaTrapez(double a, double b, unsigned int n, double (*pf)(double));
-
-
-#endif
